7cf4e163504592ea1aeb5eac63b72e77c859f47c
lakshmi narasimha <mlakshminarasimh95a@gmail.com>
2025-04-22 10:52:05 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Matrix3 from './Matrix3'
+
+const App = () => {
+  return (
+    <div>
+      <Matrix3/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Matrix3.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Matrix3 = () => {
+  const size = 3;
+  const total = size * size;
+  const lastBoxIndex = total - 1;
+
+  const [colors, setColors] = useState(Array(total).fill("white"));
+  const [clickSequence, setClickSequence] = useState([]);
+
+  const handleBoxClick = (index) => {
+    // If it's the last box
+    if (index === lastBoxIndex) {
+      clickSequence.forEach((clickedIndex, i) => {
+        setTimeout(() => {
+          setColors((prevColors) => {
+            const updated = [...prevColors];
+            updated[clickedIndex] = "orange";
+            return updated;
+          });
+        }, i * 200); // delay for animation effect
+      });
+      return;
+    }
+
+    // If not already clicked
+    if (!clickSequence.includes(index)) {
+      const updatedColors = [...colors];
+      updatedColors[index] = "green";
+      setColors(updatedColors);
+      setClickSequence([...clickSequence, index]);
+    }
+  };
+  return (
+    <div className="main">
+      
+      <div className="grid_container">
+      <h1>Welcome to Matrix Game</h1>
+      <div className="grid">
+      {Array.from({ length: total }).map((_, index) => (
+        <div
+          key={index}
+          style={{backgroundColor:colors[index],transition: "background-color 100ms"}}
+          onClick={() => handleBoxClick(index)}
+          className="box"
+        >{index + 1}</div>
+      ))}
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Matrix3;
